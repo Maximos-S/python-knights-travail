9fab380a5cd05a40fb5ce214538d5c693e8734ea
Jhonathan Ade <ade.jhonathan24@gmail.com>
2020-12-04 16:50:28 -0500

    finished phase 2

--- path_finders.py
+++ path_finders.py
@@ -4,7 +4,7 @@ from tree import Node
 class KnightPathFinder():
     def __init__(self, root):
         self._root = root
-        self._considered_positions = {root}
+        self._considered_positions = set(root)
 
     def get_valid_moves(self, pos):
         moves = []
@@ -18,13 +18,20 @@ class KnightPathFinder():
         moves.append((pos[0]-1, pos[1]+2))
         moves.append((pos[0]-1, pos[1]-2))
 
-        moves = [move for move in moves if (move[0] < 9 and move[0] > -1) and (move[1] < 9 and move[1] > -1)]
+        moves = {move for move in moves if (move[0] < 9 and move[0] > -1) and (move[1] < 9 and move[1] > -1)}
         return moves
 
     def new_move_positions(self, pos, ):
-        self.get_valid_moves(pos)
+        validMoves = self.get_valid_moves(pos).difference(self._considered_positions)
+        self._considered_positions = self._considered_positions.union(validMoves)
+        # print(self._considered_positions)
+        # print(validMoves)
+        return validMoves 
 
-knight_moves = KnightPathFinder((0, 0))
 
-print(knight_moves.get_valid_moves((8, 8)))
+# knight_moves = KnightPathFinder((6, 7))
 
+# print(knight_moves.new_move_positions((8, 8)))
+
+finder = KnightPathFinder((0, 0))
+print(finder.new_move_positions((0, 0))) 

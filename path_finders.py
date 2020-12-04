ef3e21f17372c2f9e548cd34a977d65ad72fc75e
Maximos Salzman <maximossalzman@gmail.com>
2020-12-04 16:00:57 -0500

    alskjfas;

--- path_finders.py
+++ path_finders.py
@@ -1,4 +1,4 @@
-from tree.py import Node
+from tree import Node
 
 
 class KnightPathFinder():
@@ -9,14 +9,22 @@ class KnightPathFinder():
     def get_valid_moves(self, pos):
         moves = []
 
-        moves.append(pos[0]+2, pos[1]+1)
-        moves.append(pos[0]+2, pos[1]-1)
-        moves.append(pos[0]-2, pos[1]+1)
-        moves.append(pos[0]-2, pos[1]-1)
-        moves.append(pos[0]+1, pos[1]+2)
-        moves.append(pos[0]+1, pos[1]-2)
-        moves.append(pos[0]-1, pos[1]+2)
-        moves.append(pos[0]-1, pos[1]-2)
+        moves.append((pos[0]+2, pos[1]+1))
+        moves.append((pos[0]+2, pos[1]-1))
+        moves.append((pos[0]-2, pos[1]+1))
+        moves.append((pos[0]-2, pos[1]-1))
+        moves.append((pos[0]+1, pos[1]+2))
+        moves.append((pos[0]+1, pos[1]-2))
+        moves.append((pos[0]-1, pos[1]+2))
+        moves.append((pos[0]-1, pos[1]-2))
 
-        moves = [move for move in moves if (moves[0] =< 8 and moves[0] => 0) and (moves[1] =< 8 and moves[1] => 0)]
+        moves = [move for move in moves if (move[0] < 9 and move[0] > -1) and (move[1] < 9 and move[1] > -1)]
         return moves
+
+    def new_move_positions(self, pos, ):
+        self.get_valid_moves(pos)
+
+knight_moves = KnightPathFinder((0, 0))
+
+print(knight_moves.get_valid_moves((8, 8)))
+
